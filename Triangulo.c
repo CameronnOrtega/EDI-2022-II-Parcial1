b8fa11b456db5d8cf96931d8b227cbb017885c41
Cameronn Ortega <a343906@alumnos.uaslp.mx>
2022-02-16 14:35:32 -0600

    Tarea 2 Ejercicio 2

---
+++ Triangulo.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+   float a, b, c;
+
+
+    printf("Medida del lado A: ");
+    scanf("%f", &a);
+    printf("Medida del lado B: ");
+    scanf("%f", &b);
+    printf("Medida del lado C: ");
+    scanf("%f", &c);
+
+    if(a==b && b==c && a==c)
+    {
+        printf("El triangulo es equilatero");
+    }
+    else if(b==c && a!=b || c==a && c!=b || a==b && b!=c)
+    {
+        printf("El triangulo es isosceles");
+    }
+    else if(a!=b && b!=c && a!=c)
+    {
+        printf("El triangulo es escaleno");
+    }
+    
+    return 0;
+}
