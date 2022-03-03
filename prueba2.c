2f95f53a601ef91a615f2b24ce868be05f247eea
Cameronn Ortega <a343906@alumnos.uaslp.mx>
2022-03-02 23:30:49 -0600

    Ejercicio 2 tarea 7

--- prueba2.c
+++ prueba2.c
@@ -1,44 +1,82 @@
 #include <stdio.h>
-#define n 100
-void IntroduceCadena(char *S);
-void IntroduceCaracter(char *C);
-int NumeroVeces(char *cadena, char caracter);
+#define L 4 // la longitud del arreglo
+
+void capturarDatos(int *ent);
+int imprimeSumaPosicionesPares(int *ent);
+int imprimePares(int *ent);
+void imprimeSuma(int *ent, int *suma, int *contador);
 
 int main()
 {
-    char S[n];
-    char C;
-    int numeroVecescaracter;
+    int enteros[L];
+    int sumaPosicionesPares;
+    int sumaPares;
+    int suma = 0, contador = 0;
 
-    IntroduceCadena(S);
-    IntroduceCaracter(&C);
-    numeroVecescaracter = NumeroVeces(S,C);
+    capturarDatos(enteros);
+    sumaPosicionesPares = imprimeSumaPosicionesPares(enteros);
+    sumaPares = imprimePares(enteros);
+    imprimeSuma(enteros, &suma, &contador);
 
-    printf("el caracter %c aparece %d veces en la cadena",C,numeroVecescaracter);
+    printf("La suma de las posiciones pares es: %d\n",sumaPosicionesPares);
+    printf("La suma de los numero pares es: %d\n",sumaPares);
+    printf("La suma total sin sobrepasar 100 es: %d\nEl total de numeros sumados son: %d\n", suma, contador);
+
 
     return 0;
 }
-void IntroduceCaracter(char *C)
-{
-    printf("Introduce el caracter deseado: ");
-    scanf(" %s", C);
-}
 
-void IntroduceCadena(char *S)
+void imprimeSuma(int *ent, int *suma, int *contador)
 {
-
-    printf("Introduzca  una cadena de caracteres: ");
-    gets(S);
-}
-int NumeroVeces(char *cadena, char caracter)
-{
-    int cont = 0;
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < L || *suma > 100; i++)
     {
-        if (cadena[i] == caracter)
+        *suma = *suma + ent[i];
+        *contador = 1 + *contador;
+        if(*suma > 100)
         {
-            cont++;
+            *suma = *suma - ent[i];
+            *contador = *contador - 1;
         }
     }
-    return cont;
+
+
+}
+
+int imprimePares(int *ent)
+{
+    int suma = 0;
+    for (int i = 0; i < L; i++)
+    {
+        if (ent[i] % 2 == 0)
+        {
+            suma = suma + ent[i];
+        }
+    }
+    return suma;
+}
+
+int imprimeSumaPosicionesPares(int *ent)
+{
+    int suma = 0;
+    for (int i = 0; i < L; i = i+2)
+    {
+        suma = ent[i] + suma;
+    }
+    return suma;
+}
+
+void capturarDatos(int *ent)
+{
+    for (int i = 0; i <= L; i++)
+    {
+        printf("Dame un numero entre el 1 y 100: ");
+        scanf("%d", &ent[i]);
+
+        while (ent[i] < 0 || ent[i] > 100)
+        {
+            printf("Error, valor no valido");
+            printf("\nDame un numero entre el 1 y 100: ");
+            scanf("%d", &ent[i]);
+        }
+    }
 }
